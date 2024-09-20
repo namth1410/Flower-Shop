30e14496151dcb4094c2fb713806ca88a01bd709
namth1410 <tranhainam1410@gmail.com>
2024-09-20 20:30:42 +0700

    asdsad

--- DATN/DATN/assets/js/shop.js
+++ DATN/DATN/assets/js/shop.js
@@ -532,4 +532,190 @@ document.addEventListener("DOMContentLoaded", function () {
       // Fetch dữ liệu dựa trên tag, hoặc điều hướng tới một trang khác.
     });
   });
+
+  const form = document.getElementById("price-filter-form");
+
+  form.addEventListener("submit", function (event) {
+    event.preventDefault(); // Ngăn chặn hành động submit mặc định
+    const formData = new FormData(form);
+    const data = {};
+
+    formData.forEach((value, key) => {
+      data[key] = value;
+    });
+
+    console.log(data);
+    // Tách giá trị giá
+    const priceRange = data.text.split(" - ");
+    const minCost = parseFloat(priceRange[0].replace(/[^0-9.-]+/g, "")); // Chuyển đổi và nhân với 25000
+    const maxCost = parseFloat(priceRange[1].replace(/[^0-9.-]+/g, "")); // Chuyển đổi và nhân với 25000
+
+    // Gửi yêu cầu GET tới API
+    const apiUrl = `http://namth.muotacademy.com:8080/api/flowers/price`;
+
+    fetch(apiUrl, {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({
+        minCost: minCost,
+        maxCost: maxCost,
+      }),
+    })
+      .then((response) => response.json())
+      .then((data) => {
+        // Clear existing tbody content
+
+        shopWrapper.innerHTML = "";
+        if (data.message) {
+          return;
+        }
+        data.forEach((item) => {
+          const productItem = `
+        <div class="col-md-6 col-sm-6 col-lg-4 col-custom product-area">
+        <div class="product-item">
+          <div class="single-product position-relative mr-0 ml-0">
+            <div class="product-image">
+              <a class="d-block" href="product-details.html?id=${
+                item.idflower
+              }">
+                <img
+                  src="assets/images/product/${
+                    item.image?.[0]?.image_source ?? 1
+                  }.jpg"
+                  alt=""
+                  class="product-image-1 w-100"
+                />
+              </a>
+              <span class="onsale">Sale!</span>
+              <div
+                class="add-action d-flex flex-column position-absolute"
+              >
+                <a href="compare.html" title="Compare">
+                  <i
+                    class="lnr lnr-sync"
+                    data-toggle="tooltip"
+                    data-placement="left"
+                    title="Compare"
+                  ></i>
+                </a>
+                <a href="wishlist.html" title="Add To Wishlist">
+                  <i
+                    class="lnr lnr-heart"
+                    data-toggle="tooltip"
+                    data-placement="left"
+                    title="Wishlist"
+                  ></i>
+                </a>
+                <a
+                  href="#exampleModalCenter"
+                  title="Quick View"
+                  data-bs-toggle="modal"
+                  data-bs-target="#exampleModalCenter"
+                >
+                  <i
+                    class="lnr lnr-eye"
+                    data-toggle="tooltip"
+                    data-placement="left"
+                    title="Quick View"
+                  ></i>
+                </a>
+              </div>
+            </div>
+            <div class="product-content">
+              <div class="product-title">
+                <h4 class="title-2">
+                  <a href="product-details.html?id=${item.idflower}"
+                    >${item.name}</a
+                  >
+                </h4>
+              </div>
+              <div class="product-rating">
+                <i class="fa fa-star"></i>
+                <i class="fa fa-star"></i>
+                <i class="fa fa-star"></i>
+                <i class="fa fa-star-o"></i>
+                <i class="fa fa-star-o"></i>
+              </div>
+              <div class="price-box">
+                <span class="regular-price">$${item.cost}</span>
+                <span class="old-price"><del>$70.00</del></span>
+              </div>
+              <a href="cart.html" class="btn product-cart"
+                >Add to Cart</a
+              >
+            </div>
+            <div class="product-content-listview">
+              <div class="product-title">
+                <h4 class="title-2">
+                  <a href="product-details.html?id=${item.idflower}"
+                    >${item.name}</a
+                  >
+                </h4>
+              </div>
+              <div class="product-rating">
+                <i class="fa fa-star"></i>
+                <i class="fa fa-star"></i>
+                <i class="fa fa-star"></i>
+                <i class="fa fa-star-o"></i>
+                <i class="fa fa-star-o"></i>
+              </div>
+              <div class="price-box">
+                <span class="regular-price">$${item.cost}</span>
+                <span class="old-price"><del>$70.00</del></span>
+              </div>
+              <p class="desc-content">
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+                Fusce posuere metus vitae arcu imperdiet, id aliquet
+                ante scelerisque. Sed sit amet sem vitae urna fringilla
+                tempus.
+              </p>
+              <div class="button-listview">
+                <a
+                  href="cart.html"
+                  class="btn product-cart button-icon flosun-button dark-btn"
+                  data-toggle="tooltip"
+                  data-placement="top"
+                  title="Add to Cart"
+                >
+                  <span>Add to Cart</span>
+                </a>
+                <a
+                  class="list-icon"
+                  href="compare.html"
+                  title="Compare"
+                >
+                  <i
+                    class="lnr lnr-sync"
+                    data-toggle="tooltip"
+                    data-placement="top"
+                    title="Compare"
+                  ></i>
+                </a>
+                <a
+                  class="list-icon"
+                  href="wishlist.html"
+                  title="Add To Wishlist"
+                >
+                  <i
+                    class="lnr lnr-heart"
+                    data-toggle="tooltip"
+                    data-placement="top"
+                    title="Wishlist"
+                  ></i>
+                </a>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+              `;
+          shopWrapper.innerHTML += productItem;
+        });
+      })
+      .catch((error) => {
+        console.error("Error:", error); // Xử lý lỗi
+      });
+  });
 });
